cef5288eeaaba8bc93f67117520bf10553d6b8f6
brabravol <bra.bravol@duocuc.cl>
2026-05-01 17:13:05 -0400

    creacion de controladores admin

--- src/main/java/com/smart_solutions_auth/API/controller/user/UserRoleController.java
+++ src/main/java/com/smart_solutions_auth/API/controller/user/UserRoleController.java
@@ -3,7 +3,7 @@ package com.smart_solutions_auth.API.controller.user;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smart_solutions_auth.API.dto.user.UserRoleDTO;
-import com.smart_solutions_auth.API.service.UserRoleService; // Asumiendo que el método está aquí
+import com.smart_solutions_auth.API.service.UserRoleService; 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
