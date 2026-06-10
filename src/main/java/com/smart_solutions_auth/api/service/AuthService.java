e847ffe2015509ba76e523a88197cbcb4ed478ff
BenjaminMarin4444 <bra.bravol@duocuc.cl>
2026-06-10 18:00:35 -0400

    fix

--- src/main/java/com/smart_solutions_auth/api/service/AuthService.java
+++ src/main/java/com/smart_solutions_auth/api/service/AuthService.java
@@ -39,18 +39,18 @@ public class AuthService {
 
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", token)
                 .httpOnly(true)
-                .secure(false) 
+                .secure(true) 
                 .path("/")
                 .maxAge(3600) 
-                .sameSite("Strict")
+                .sameSite("None")
                 .build();
 
         ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", refreshToken)
                 .httpOnly(true)
-                .secure(false)
+                .secure(true)
                 .path("/api/v1/auth/refresh") 
                 .maxAge(604800) 
-                .sameSite("Strict")
+                .sameSite("None")
                 .build();
 
         response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
@@ -76,7 +76,7 @@ public class AuthService {
                         .httpOnly(true)
                         .path("/")
                         .maxAge(3600)
-                        .sameSite("Strict")
+                        .sameSite("None")
                         .build();
                 response.addHeader(org.springframework.http.HttpHeaders.SET_COOKIE, cookie.toString());
                 }
