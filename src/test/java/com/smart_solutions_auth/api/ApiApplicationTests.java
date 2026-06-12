98184c054eade02e3cd3a84c7652d8005e4ec915
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-12 19:14:04 -0400

    fix: Correción CacheConfig

--- src/test/java/com/smart_solutions_auth/api/ApiApplicationTests.java
+++ src/test/java/com/smart_solutions_auth/api/ApiApplicationTests.java
@@ -2,10 +2,15 @@ package com.smart_solutions_auth.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootTest
 class ApiApplicationTests {
 
+	@MockBean
+    private RedisConnectionFactory redisConnectionFactory;
+	
 	@Test
 	void contextLoads() {
 	}
